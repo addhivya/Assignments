3468060fb9e39ed1562d9af8def8a03238df4c81
addhivya <42990125+addhivya@users.noreply.github.com>
2018-09-18 20:27:47 -0700

    Add files via upload

---
+++ Assignment2/Tester.java
@@ -0,0 +1,13 @@
+public class Tester {
+
+    public static void main(String[] args) {
+        Assignment2 assignment=new Assignment2();
+        Employee emp1 = new Employee("Divya", 59, Gender.FEMALE, 8900);
+        assignment.socialSecurityTax(emp1);
+        assignment.insuranceCoverage(emp1);
+        Employee emp2 = new Employee("Naveen", 25, Gender.MALE, 800);
+        Employee emp3 = new Employee("Rachna", 59, Gender.FEMALE, 8901);
+        assignment.sortSalary(emp1,emp2,emp3);
+        assignment.tripleSalary(emp1);
+    }
+}
