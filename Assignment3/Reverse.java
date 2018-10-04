b5bc19422ecc57946e2624fe17b077f2e2efb6d3
addhivya <42990125+addhivya@users.noreply.github.com>
2018-10-03 21:15:50 -0700

    Add files via upload
    Reverse

---
+++ Assignment3/Reverse.java
@@ -0,0 +1,25 @@
+package Assignment3;
+
+public class Reverse {
+    public String reverse(String s) {
+        System.out.println("String:" + s);
+        String[] data = s.trim().split("\\s+");
+        StringBuilder result = new StringBuilder();
+        for (int i = data.length - 1; i >= 0; i--) {
+            if (i == data.length - 1) {
+                result.append(data[i].toLowerCase());
+            } else {
+                result.append(" ");
+                result.append(data[i].toLowerCase());
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Reverse r = new Reverse();
+        String res = r.reverse("       The sky  is      blue   ");
+        System.out.print("Reverse:" + res);
+    }
+}
+
